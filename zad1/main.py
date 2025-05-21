de10c93debf76f6872b2996577c072caaa91fa08
Krystian Petrusevich <krystekvader@gmail.com>
2025-05-21 22:28:21 +0200

    kurczak2

--- zad1/main.py
+++ zad1/main.py
@@ -6,7 +6,7 @@ def kurczak(numbers):
     n = len(numbers)
     dp = [1] * n
     prev = [-1] * n
-    
+
     for i in range(1, n):
         best_j = max(
             filter(lambda j: numbers[j] < numbers[i], range(i)),
@@ -16,3 +16,13 @@ def kurczak(numbers):
         if best_j is not None:
             dp[i] = dp[best_j] + 1
             prev[i] = best_j
+    max_idx = max(range(n), key=lambda j: dp[j])
+    lis = []
+    while max_idx != -1:
+        lis.append(numbers[max_idx])
+        max_idx = prev[max_idx]
+    return lis[::-1]
+
+
+test1 = [10, 22, 9, 33, 21, 50, 41, 60, 80]
+print(kurczak(test1))
