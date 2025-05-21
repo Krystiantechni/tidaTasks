5bc2755687db9f694a9fd0db221e92c700bf7441
Krystian Petrusevich <krystekvader@gmail.com>
2025-05-21 23:01:28 +0200

    kot1

--- zad2/main.py
+++ zad2/main.py
@@ -1,1 +1,15 @@
 #2[2, -4, 6, 8, -10, 100, -6, 5]
+
+def kot(numbers):
+    
+    if not numbers:
+        return []
+    
+    max_sum = float('-inf')
+    current_sum = 0
+    start = 0
+    current_start = 0
+    max_length = 0
+    
+    for i, num in enumerate(numbers):
+        current_sum += num
