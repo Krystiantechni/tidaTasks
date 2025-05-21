adbc81e03e297986abc97bce254895111f62e778
Krystian Petrusevich <krystekvader@gmail.com>
2025-05-21 23:50:38 +0200

    pies

--- zad4/main.py
+++ zad4/main.py
@@ -1,1 +1,12 @@
 #4
+from itertools import permutations
+
+def pies(n):
+    letters = [chr(ord('a') + i) for i in range(n)]
+    result = [''.join(p) for p in permutations(letters)]
+    return sorted(result)
+
+n = 3
+lista = pies(n)
+for k in lista:
+    print(k)
